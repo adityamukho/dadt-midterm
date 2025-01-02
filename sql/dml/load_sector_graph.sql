7ec9b44bdeab99cbe3919273d4112e8b3aeac0ca
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2025-01-02 15:07:40 +0530

    Assert all necessary sectors are present.

--- sql/dml/load_sector_graph.sql
+++ sql/dml/load_sector_graph.sql
@@ -1,32 +1,55 @@
-CREATE TEMPORARY TABLE IF NOT EXISTS sector_graph_raw
+CREATE TEMPORARY TABLE IF NOT EXISTS temp_sector_graph
 (
     sector     VARCHAR(255) NOT NULL,
     sub_sector VARCHAR(255) NOT NULL
 );
 
-TRUNCATE sector_graph_raw;
+TRUNCATE temp_sector_graph;
 
 LOAD DATA LOCAL INFILE 'data/s-and-p-500-companies/data/constituents.csv'
-    IGNORE INTO TABLE sector_graph_raw
+    INTO TABLE temp_sector_graph
     FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
     IGNORE 1 LINES
-    (@Symbol, @Security, @GICS_Sector, @GICS_Sub_Industry, @Headquarters_Location, @Date_Added, @CIK, @Founded)
+    (@Symbol, @Security, @GICS_Sector, @GICS_Sub_Industry)
     SET sector = @GICS_Sector,
         sub_sector = @GICS_Sub_Industry;
 
 INSERT IGNORE INTO sector_graph (name)
-SELECT sector_graph_raw.sector
-from sector_graph_raw;
+SELECT temp_sector_graph.sector
+from temp_sector_graph;
 
 INSERT IGNORE INTO sector_graph (name)
-SELECT sector_graph_raw.sub_sector
-from sector_graph_raw;
+SELECT temp_sector_graph.sub_sector
+from temp_sector_graph;
 
 UPDATE sector_graph sg
 SET parent_id = (SELECT DISTINCT sg1.id
                  FROM sector_graph sg1
-                          INNER JOIN sector_graph_raw sgr ON sg1.name = sgr.sector
-                 WHERE sgr.sub_sector = sg.name)
+                          INNER JOIN temp_sector_graph tsg ON sg1.name = tsg.sector
+                 WHERE tsg.sub_sector = sg.name)
 WHERE parent_id IS NULL;
 
-DROP TEMPORARY TABLE IF EXISTS sector_graph_raw;
+DROP TEMPORARY TABLE IF EXISTS temp_sector_graph;
+
+CREATE TEMPORARY TABLE IF NOT EXISTS temp_sector
+(
+    sector VARCHAR(255) NOT NULL
+);
+
+TRUNCATE temp_sector;
+
+LOAD DATA LOCAL INFILE 'data/s-and-p-500-companies-financials/data/constituents.csv'
+    INTO TABLE temp_sector
+    FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+    LINES TERMINATED BY '\r\n'
+    IGNORE 1 LINES
+    (@Symbol, @Name, @Sector)
+    SET sector = @Sector;
+
+SELECT count(a.sector)
+FROM (SELECT ts.sector, sg.name
+      FROM temp_sector ts
+               LEFT OUTER JOIN sector_graph sg on ts.sector = sg.name) a
+WHERE a.name IS NULL;
+
+DROP TEMPORARY TABLE IF EXISTS temp_sector;
