5a588d0f8158b8ebd530da0a2eeb8d24a71a66ff
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2025-01-02 17:17:49 +0530

    Load data into company financials.

---
+++ sql/dml/load_company_financials.sql
@@ -0,0 +1,19 @@
+LOAD DATA LOCAL INFILE 'data/s-and-p-500-companies-financials/data/constituents-financials.csv'
+    IGNORE INTO TABLE company_financials
+    FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+    LINES TERMINATED BY '\r\n'
+    IGNORE 1 LINES
+    (@Symbol, @Name, @Sector, @Price, @Price_Earnings, @Dividend_Yield, @Earnings_Share, @52_Week_Low, @52_Week_High,
+     @Market_Cap, @EBITDA, @Price_Sales, @Price_Book, @SEC_Filings)
+    SET symbol = @Symbol,
+        as_of_date = CURRENT_DATE,
+        price = @Price,
+        price_earnings = @Price_Earnings,
+        dividend_yield = @Dividend_Yield,
+        earnings_share = @Earnings_Share,
+        fifty_two_week_low = @`52_Week_Low`,
+        fifty_two_week_high = @`52_Week_High`,
+        market_cap = @Market_Cap,
+        ebitda = @EBITDA,
+        price_sales = @Price_Sales,
+        price_book = @Price_Book;
